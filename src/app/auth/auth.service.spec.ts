2acb63e27bf3ad748ff20a3792b98e289bb07d96
Bing Soundara <bsound83@gmail.com>
2018-10-28 08:26:13 -0500

    add auth services

--- src/app/auth/auth.service.spec.ts
+++ src/app/auth/auth.service.spec.ts
@@ -1,11 +1,11 @@
-import { commonTestingModules } from '../common/common.testing';
+import { commonTestingModules, commonTestingProviders } from '../common/common.testing';
 
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
-  beforeEach(() => TestBed.configureTestingModule({ imports: commonTestingModules }));
+  beforeEach(() => TestBed.configureTestingModule({ imports: commonTestingModules, providers: commonTestingProviders }));
 
   it('should be created', () => {
     const service: AuthService = TestBed.get(AuthService);
